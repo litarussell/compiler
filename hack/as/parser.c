cd6d9604fd9f04e5ac53e32b0521edbf20707c0b
李涛 <tao.li@fengjr.com>
2020-08-13 18:26:29 +0800

    调整parser.c的代码结构, 用于解析C指令

--- hack/as/parser.c
+++ hack/as/parser.c
@@ -9,32 +9,91 @@
 #define getch if(-1 == get_ch()) return -1;
 #define nextline if(-1 == next_line()) return -1;
 
+/**
+ *  汇编token
+ */
+typedef enum COMP_TOKEN {
+  TC_ZERO,
+  TC_ONE,
+  TC_MINUS_ONE,
+  TC_D,
+  TC_A,
+  TC_M,
+  TC_NOT_D,
+  TC_NOT_A,
+  TC_NOT_M,
+  TC_MINUS_D,
+  TC_MINUS_A,
+  TC_MINUS_M,
+  TC_D_ADD_ONE,
+  TC_A_ADD_ONE,
+  TC_M_ADD_ONE,
+  TC_D_MINUS_ONE,
+  TC_A_MINUS_ONE,
+  TC_M_MINUS_ONE,
+  TC_D_ADD_A,
+  TC_D_ADD_M,
+  TC_D_MINUS_A,
+  TC_D_MINUS_M,
+  TC_A_MINUS_D,
+  TC_M_MINUS_D,
+  TC_D_AND_A,
+  TC_D_AND_M,
+  TC_D_OR_A,
+  TC_D_OR_M
+} c_token;
+
+typedef enum DEST_TOKEN {
+  TD_NULL,
+  TD_M,
+  TD_D,
+  TD_MD,
+  TD_A,
+  TD_AM,
+  TD_AD,
+  TD_AMD
+} d_token;
+
+typedef enum JUMP_TOKEN {
+  T_NULL,
+  T_JGT,
+  T_JEQ,
+  T_JGE,
+  T_JLT,
+  T_JNE,
+  T_JLE,
+  T_JMP
+} j_token;
+
 typedef enum COMMOND_TYPE {
-  A_COMMOND,
+  A_COMMOND_ID,
+  A_COMMOND_NUM,
   C_COMMOND,
   L_COMMOND,
   NOTE,
 } c_type;
 
-char tmp[maxsym + 1]; // 临时符号, 多出的一个字节用于存放0
-char id[maxsym + 1];  // 当前变量符号
 
-int sym;
+
+FILE* fin;            // 
+int  num;             // 立即数
+int  code;            // 从A指令中提取的符号地址、或者立即数
+int  pc = -1;         // 程序计数器 每遇到A或C指令+1
+char ch = ' ';        // 获取字符的缓冲区
+char line[81];        // 读取行缓冲区
+char id[maxsym + 1];  // 当前变量符号
+char tmp[maxsym + 1]; // 临时符号, 多出的一个字节用于存放0
+
 c_type sym_type;
-int code;         // 从A指令中提取的符号地址、或者立即数
-char ch = ' ';    // 获取字符的缓冲区
-char line[81];    // 读取行缓冲区
-FILE* fin;        // 
 
 int i = 0;
 int j = 0;
-int pc = -1;      // 程序计数器 每遇到A或C指令+1
 
 // 提取@xxx
 // 或提取 (xxx) 中的xxx
 int symbol(c_type type) {
   getch;
-  if (type == A_COMMOND) { // a指令符号
+  if (type == A_COMMOND_ID) { // a指令符号
 
   }
   else if (type == L_COMMOND) { // 
@@ -44,12 +103,58 @@ int symbol(c_type type) {
 
 // 处理当前C指令的dest助记符
 d_token dest_token() {
-
+  if (ch == 'A') {
+    getch;
+    if (ch == 'M') {
+      getch;
+      if (ch == 'D') {
+        getch;
+        return TD_AMD;
+      }
+      return TD_AM;
+    }
+    else if (ch == 'D') {
+      getch;
+      return TD_AD;
+    }
+    return TD_A;
+  }
+  else if (ch == 'M') {
+    getch;
+    if (ch == 'D') {
+      getch;
+      return TD_MD;
+    }
+    return TD_M;
+  }
+  else if (ch == 'D') {
+    getch;
+    return TD_D;
+  }
+  else {
+    return TD_NULL;
+  }
 }
 
 // 处理当前C指令的comp助记符
 c_token comp_token() {
-  
+  bool MINUS_FLAG = false;
+  bool NOT_FLAG = false;
+  if (ch == '-') MINUS_FLAG = true;
+  else if (ch == '!') NOT_FLAG = true;
+  getch;
+  if (ch == 'A') {
+    getch;
+    if (ch == 'A') {
+
+    }
+  }
+  else if (ch == 'D') {
+
+  }
+  else if (ch == 'M') {
+
+  }
 }
 
 // 处理当前C指令的jump助记符
@@ -87,7 +192,7 @@ int get_ch() {
 // 获取下一个token
 int get_token() {
   while (ch == ' ' || ch == '\n' || ch == '\r' || ch == '\t') {
-    getch
+    getch;
   }
 
   if (ch == '/') {
@@ -113,25 +218,16 @@ int get_token() {
               || (ch >= 'A' && ch <= 'Z'));
       tmp[k] = 0;
       strcpy(id, tmp);
-      // 搜索符号地址
-      int addr = get_address(id);
-      
-      if (addr == -1) {
-        code = add_entry(id, -1);   // 创建符号
-      } else {
-        code = addr;
-      }
-      sym_type = A_COMMOND;
-      pc++;
+      sym_type = A_COMMOND_ID;
       return 0;
     }
 
     // 立即数
     if (ch >= '0' && ch <= '9') {
       int k = 0;
-      int num = 0;
+      int n = 0;
       do { // 拼接数字
-        num = num * 10 + ch - '0';
+        n = n * 10 + ch - '0';
         k++;
         getch;
       } while ((ch >= 'a' && ch <= 'z') || (ch>='0' && ch<='9')
@@ -140,11 +236,12 @@ int get_token() {
       if (k > maxsym) {
         return -1;
       }
-      code = num;
-      sym_type = A_COMMOND;
-      pc++;
+      num = n;
+      sym_type = A_COMMOND_NUM;
       return 0;
     }
+
+    return -1;
   }
   else {
     if (ch == '(') {  // 伪命令标签
@@ -163,15 +260,16 @@ int get_token() {
         if (ch != ')') {
           return -1;
         }
-
         strcpy(id, tmp);
-
-        add_entry(id);
+        sym_type = L_COMMOND;
       }
 
-      sym_type = L_COMMOND;
+      return -1;
     }
     else { // C指令
+      d_token d = dest_token();
+      c_token c = comp_token();
+      j_token j = jump_token();
       pc++;
       sym_type = C_COMMOND;
     }
@@ -184,16 +282,31 @@ void parser() {
   while (get_token() != -1) {
     printf("%u", sym_type);
 
-    if (sym_type == A_COMMOND) {  // 处理A指令
+    if (sym_type == A_COMMOND_ID) {  // 处理A指令-符号
+      // 搜索符号地址
+      int addr = get_address(id);
+      
+      if (addr == -1) {
+        code = add_entry(id, -1);   // 创建符号
+      } else {
+        code = addr;
+      }
       printf("%d\n", code);
+      pc++;
+    }
+    else if (sym_type == A_COMMOND_NUM) { // 处理A指令-立即数
+      code = num;
+      printf("%d\n", code);
+      pc++;
     }
     else if (sym_type == L_COMMOND) {  // 处理伪命令
-      int code = symbol(L_COMMOND);
+      printf("%s\n", id);
+      // add_entry(id);
+      // int code = symbol(L_COMMOND);
     }
     else if (sym_type == C_COMMOND) {  // 处理C指令
-      d_token d = dest_token();
-      c_token c = comp_token();
-      j_token j = jump_token();
+      printf("%d\n", sym_type);
+      pc++;
     }
   }
   fclose(fin);
